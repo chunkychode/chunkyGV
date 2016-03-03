76cf2490b4344c3ef0ad286f00a12449131387d4
adam <adam@chunkychode.com>
2016-03-02 19:43:24 -0800

    sdfsdfsdf

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -19,7 +19,7 @@ namespace ConsoleApplication1
             Console.WriteLine("added bom 3 on b2");
             //on master2dcvbcvb
             //should work now
-            //on be23ed
+            //on be23edsdfgdsfsdfsdf
             //sdfsdfsdfs
         }
     }
